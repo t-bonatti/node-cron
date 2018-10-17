491b27a0690764f0f401f929972a116723404879
Tiago Bonatti <tiago.bonatti@gmail.com>
2018-10-16 21:26:53 -0300

    Emit events on finished task

--- src/task.js
+++ src/task.js
@@ -1,27 +1,19 @@
 'use strict';
 
-const events = require('events');
-const util = require('util');
+const EventEmitter = require('events');
 
-class Task{
+class Task extends EventEmitter{
     constructor(execution){
+        super();
+
         if(typeof execution !== 'function'){
             throw 'execution must be a function';
         }
 
-        events.EventEmitter.call(this);
-
-        this.execute = (now) => {
-            // TODO: Handle execution
-            // check if execution returns a promise
-            // emit events on start and on finished
-            // force execution return a promise
-            execution(now);
-        };
+        this.execute = (now) => Promise.resolve(execution(now))
+            .then((value) => this.emit('task-finished', value));
     }
 }
-
-util.inherits(Task, events.EventEmitter);
 
 module.exports = Task;
 
